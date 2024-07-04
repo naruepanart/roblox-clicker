5954a76bb861703c4c361e9adc34ffc083b5aa8c
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-07-04 17:28:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
-
 	"github.com/go-vgo/robotgo"
 )
 
@@ -18,22 +17,24 @@ var (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Auto Clicker")
+	w := a.NewWindow("Roblox Auto Click by nptfreez")
 
 	// Create buttons and set their functionality
-	startButton := widget.NewButton("Start (F2)", func() {
+	startButton := widget.NewButton("Start (F1)", func() {
 		startAutoClick()
 	})
-	stopButton := widget.NewButton("Exit (F3)", func() {
+	stopButton := widget.NewButton("Stop (F2)", func() {
 		stopAutoClick()
 	})
 
 	// Define keyboard shortcuts
 	w.Canvas().SetOnTypedKey(func(event *fyne.KeyEvent) {
-		if event.Name == fyne.KeyF2 {
+		switch event.Name {
+		case fyne.KeyF1:
 			startAutoClick()
-		}
-		if event.Name == fyne.KeyF3 {
+		case fyne.KeyF2:
+			stopAutoClick()
+		case fyne.KeyF3:
 			stopAutoClick()
 			a.Quit() // Quit application on F3 press
 		}
@@ -41,7 +42,7 @@ func main() {
 
 	// Layout using VBox container
 	w.SetContent(container.NewVBox(
-		widget.NewLabel("Roblox Auto Clicker"),
+		widget.NewLabel("Roblox Auto Click by nptfreez"),
 		startButton,
 		stopButton,
 	))
@@ -51,19 +52,19 @@ func main() {
 
 func startAutoClick() {
 	if stopChan != nil {
-		fmt.Println("Auto clicker already running.")
+		fmt.Println("Auto Click already running")
 		return
 	}
 	stopChan = make(chan struct{})
 	go autoClick()
-	fmt.Println("Auto clicker started.")
+	fmt.Println("Auto Click started.")
 }
 
 func stopAutoClick() {
 	if stopChan != nil {
 		close(stopChan)
 		stopChan = nil
-		fmt.Println("Auto clicker stopped.")
+		fmt.Println("Auto Click stopped")
 	}
 }
 
@@ -71,14 +72,14 @@ func autoClick() {
 	for {
 		select {
 		case <-stopChan:
-			fmt.Println("Auto clicker stopped.")
+			fmt.Println("Auto Click stopped")
 			return
 		default:
 			// Perform left click
 			robotgo.Click("left")
 
-			// Delay for 1 second
-			time.Sleep(500 * time.Microsecond)
+			// Delay for 500 milliseconds (adjust as needed)
+			time.Sleep(500 * time.Millisecond)
 		}
 	}
 }
